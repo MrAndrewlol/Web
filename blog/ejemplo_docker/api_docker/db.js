9a5293619a6190f1426f62f0a98958bbe19cfef9
MrAndrewlol <andrewlol774@gmail.com>
2024-03-07 13:20:28 -0600

    npm lint

--- blog/ejemplo_docker/api_docker/db.js
+++ blog/ejemplo_docker/api_docker/db.js
@@ -1,59 +1,52 @@
-
 import conn from './conn.js'
 
 export async function getAllPosts() {
-    try {
-        const [rows] = await conn.query('SELECT * FROM blogs')
-        return rows
-
-    } catch (e) {
-        console.log(e)
-        return e
-    }
+  try {
+    const [rows] = await conn.query('SELECT * FROM blogs')
+    return rows
+  } catch (e) {
+    console.log(e)
+    return e
+  }
 }
 
 export async function getPosts(id) {
-    try{
-        console.log(id)
-        const [rows] = await conn.query(`SELECT * FROM blogs WHERE id = ${id}`)
-        return rows
-    }catch(e){
-        console.log(e)
-        return e
-    }
-
-}  
-
-export async function createPost(title, content) {
-    try {
-        const [rows] = await conn.query(`INSERT INTO blogs (title, content) VALUES ('${title}', '${content}')`)
-        return rows
-
-    } catch (e) {
-        console.log(e)
-        return e
-    }
- }
-
-
-export async function deletepost(id) {
-    try {
-        const [rows] = await conn.query(`DELETE FROM blogs WHERE id = ${id}`)
-        return rows
-
-    } catch (e) {
-        console.log(e)
-        return e
-    }
+  try {
+    console.log(id)
+    const [rows] = await conn.query(`SELECT * FROM blogs WHERE id = ${id}`)
+    return rows
+  } catch (e) {
+    console.log(e)
+    return e
+  }
 }
 
-export async function putpost(id, title ,content) {
-    try {
-        const [rows] = await conn.query(` UPDATE blogs SET title = '${title}', content = '${content}' WHERE id = '${id}'`)
-        return rows
+export async function createPost(title, content) {
+  try {
+    const [rows] = await conn.query(`INSERT INTO blogs (title, content) VALUES ('${title}', '${content}')`)
+    return rows
+  } catch (e) {
+    console.log(e)
+    return e
+  }
+}
 
-    } catch (e) {
-        console.log(e)
-        return e
-    }
+export async function deletepost(id) {
+  try {
+    const [rows] = await conn.query(`DELETE FROM blogs WHERE id = ${id}`)
+    return rows
+  } catch (e) {
+    console.log(e)
+    return e
+  }
+}
+
+export async function putpost(id, title, content) {
+  try {
+    const [rows] = await conn.query(` UPDATE blogs SET title = '${title}', content = '${content}' WHERE id = '${id}'`)
+    return rows
+  } catch (e) {
+    console.log(e)
+    return e
+  }
 }
--- blog/ejemplo_docker/api_docker/index.js
+++ blog/ejemplo_docker/api_docker/index.js
@@ -1,14 +1,14 @@
 import express from 'express'
 // import cors from 'cors'
-import { getAllPosts, getPosts, createPost, deletepost, putpost } from './db.js'
-
+import {
+  getAllPosts, getPosts, createPost, deletepost, putpost,
+} from './db.js'
 
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-
 })
 
 app.listen(port, () => {
@@ -18,39 +18,34 @@ app.listen(port, () => {
 app.use(express.json())
 
 app.get('/posts', async (req, res) => {
-
   const posts = await getAllPosts()
   res.json(posts)
 })
 
-
 app.get('/posts/:id', async (req, res) => {
-  const id = req.params.id
+  const { id } = req.params
   const posts = await getPosts(id)
   res.json(posts)
 })
 
-
 app.post('/posts', async (req, res) => {
   const [title, content] = [req.body.title, req.body.content]
-  console.log(title, content);
+  console.log(title, content)
   const blogs = await createPost(title, content)
   res.json(blogs)
 })
 
-app.delete('/posts/:id', async(req, res)=>{
-  console.log('delete blog');
-  const id = req.params.id
-  console.log(id);
+app.delete('/posts/:id', async (req, res) => {
+  console.log('delete blog')
+  const { id } = req.params
+  console.log(id)
   const result = await deletepost(id)
   res.json(result)
 })
 
-
-app.put('/posts/:id', async(req, res) =>{
-  const id = req.params.id
+app.put('/posts/:id', async (req, res) => {
+  const { id } = req.params
   const [title, content] = [req.body.title, req.body.content]
   const result = await putpost(id, title, content)
   res.json(result)
-
 })
