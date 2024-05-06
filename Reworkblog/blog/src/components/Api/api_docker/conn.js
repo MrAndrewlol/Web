d52219fa58a8401d932063b7cdf110d6543011ab
cmd-AJ <Andrewlol774@gmail.com>
2024-05-06 02:15:19 -0600

    rework

--- Reworkblog/blog/src/components/Api/api_docker/conn.js
+++ Reworkblog/blog/src/components/Api/api_docker/conn.js
@@ -2,11 +2,8 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const conn = new  Pool({
-  user: process.env.db_user,
-  host: process.env.db_host, //SUSTITUIR EL RESTO DE VARIABLES DE ENTORNO
-  database: process.env.db_database,
-  password: process.env.db_password,
-  port: 5432,
+  url: 
+  database: 'carblog'
 
 })
 
